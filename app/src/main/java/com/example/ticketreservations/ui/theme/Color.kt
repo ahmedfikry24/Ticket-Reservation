1671d482adc7e385cc32c5c8dc847b9539d71ed7
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-03 03:28:32 +0300

    add colors & update theme

--- app/src/main/java/com/example/ticketreservations/ui/theme/Color.kt
+++ app/src/main/java/com/example/ticketreservations/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.ticketreservations.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val white = Color(0xFFffffff)
+val black = Color(0xFF000000)
+val navy = Color(0xFF131f36)
+val grey = Color(0xFF404040)
+val lightGrey = Color(0xFF928a7d)
+val orange = Color(0xFFff5524)
+val primaryTextColor = Color(0xFF060b0c)
+val secondaryTextColor = Color(0xFF2f2f2f)
---
+++ app/src/main/java/com/example/ticketreservations/ui/theme/fonts.kt
@@ -0,0 +1,12 @@
+package com.example.ticketreservations.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.ticketreservations.R
+
+val rubik = FontFamily(
+    Font(R.font.rubik_regular, weight = FontWeight.Normal),
+    Font(R.font.rubik_medium, weight = FontWeight.Medium),
+    Font(R.font.rubik_semibold, weight = FontWeight.SemiBold),
+)
