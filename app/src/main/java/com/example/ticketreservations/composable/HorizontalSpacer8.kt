ef07268f657d6dfb8ecca2415f48a0c997d7298e
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-04 01:24:25 +0300

    add spacers

---
+++ app/src/main/java/com/example/ticketreservations/composable/HorizontalSpacer8.kt
@@ -0,0 +1,12 @@
+package com.example.ticketreservations.composable
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalSpacer8(){
+    Spacer(modifier = Modifier.width(8.dp))
+}
