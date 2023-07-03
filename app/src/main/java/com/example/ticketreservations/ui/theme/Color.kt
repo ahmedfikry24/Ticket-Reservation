297a71a5adcbbd5938342679ea7b82b43c8a425f
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-04 01:22:56 +0300

    update colors & strings

--- app/src/main/java/com/example/ticketreservations/ui/theme/Color.kt
+++ app/src/main/java/com/example/ticketreservations/ui/theme/Color.kt
@@ -3,6 +3,8 @@ package com.example.ticketreservations.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val white = Color(0xFFffffff)
+val white87 = Color(0xDEFFFFFF)
+val white60 = Color(0x99FFFFFF)
 val black = Color(0xFF000000)
 val navy = Color(0xFF131f36)
 val grey = Color(0xFF404040)
