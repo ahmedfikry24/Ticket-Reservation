58241f5a6319cbac7a9ee735114418438f71ba75
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-06 00:19:26 +0300

    update spacers

--- app/src/main/java/com/example/ticketreservations/composable/HorizontalSpacer.kt
+++ app/src/main/java/com/example/ticketreservations/composable/HorizontalSpacer.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
 
 @Composable
-fun HorizontalSpacer8(){
-    Spacer(modifier = Modifier.width(8.dp))
+fun HorizontalSpacer(width:Dp){
+    Spacer(modifier = Modifier.width(width))
 }
--- app/src/main/java/com/example/ticketreservations/composable/VerticalSpacer.kt
+++ app/src/main/java/com/example/ticketreservations/composable/VerticalSpacer.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
 
 @Composable
-fun VerticalSpacer16() {
-    Spacer(modifier = Modifier.height(16.dp))
+fun VerticalSpacer(height:Dp) {
+    Spacer(modifier = Modifier.height(height))
 }
