062cfd1123b88e752f0ffb19622bd19439902dd1
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-06 18:07:37 +0300

    add HomeViewModel

---
+++ app/src/main/java/com/example/ticketreservations/screens/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ticketreservations.screens.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+@HiltViewModel
+class HomeViewModel : ViewModel() {
+    private val _state = MutableStateFlow(HomeUiState())
+    val state: StateFlow<HomeUiState>
+        get() = _state
+}
