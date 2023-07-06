3dc37f4d6f09f616f80370fed0df4602beec3cb4
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-06 20:39:11 +0300

    update HomeScreen

--- app/src/main/java/com/example/ticketreservations/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/ticketreservations/screens/home/HomeScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -29,6 +29,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.ticketreservations.AppDestination
 import com.example.ticketreservations.R
 import com.example.ticketreservations.composable.HorizontalSpacer
 import com.example.ticketreservations.composable.IconMovieTime
@@ -44,18 +46,22 @@ import kotlin.math.absoluteValue
 
 @Composable
 fun HomeScreen(
+    paddingValues: PaddingValues,
+    navController: NavController,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
     HomeContent(
+        paddingValues = paddingValues,
         state = state,
-        onClickMovie = {}
+        onClickMovie = { navController.navigate(AppDestination.MovieInfo.screen) }
     )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun HomeContent(
+    paddingValues: PaddingValues,
     state: HomeUiState,
     onClickMovie: () -> Unit
 ) {
@@ -67,59 +73,54 @@ private fun HomeContent(
         ), endY = tileSize, tileMode = TileMode.Clamp
     )
 
-    Column(
+    val pagerState = rememberPagerState(2)
+    val imageSize = LocalConfiguration.current.screenWidthDp / 1.5
+    HorizontalPager(
         modifier = Modifier
             .fillMaxSize()
-            .background(brushBackground),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val pagerState = rememberPagerState(2)
-        val imageSize = LocalConfiguration.current.screenWidthDp / 1.5
-        HorizontalPager(
-            modifier = Modifier.fillMaxWidth(),
-            pageCount = state.movies.size,
-            state = pagerState,
-            contentPadding = PaddingValues(16.dp),
-            beyondBoundsPageCount = 2,
-        ) { page ->
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                VerticalSpacer(height = 40.dp)
-                MovieStatus(isShowingNow = state.movies[page].isShowingNow)
-                VerticalSpacer(height = 20.dp)
-                ImageFromUrl(
-                    url = state.movies[page].imageUrl,
-                    modifier = Modifier
-                        .size(imageSize.dp, 400.dp)
-                        .clip(RoundedCornerShape(20.dp))
-                        .graphicsLayer {
-                            val pageOffset =
-                                ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
-                            alpha = lerp(
-                                start = 0.5f,
-                                stop = 1f,
-                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                            ).also {
-                                scaleX = it
-                                scaleY = it
-                            }
+            .background(brushBackground)
+            .padding(paddingValues),
+        pageCount = state.movies.size,
+        state = pagerState,
+        contentPadding = PaddingValues(16.dp),
+        beyondBoundsPageCount = 2,
+    ) { page ->
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            VerticalSpacer(height = 40.dp)
+            MovieStatus(isShowingNow = state.movies[page].isShowingNow)
+            VerticalSpacer(height = 20.dp)
+            ImageFromUrl(
+                url = state.movies[page].imageUrl,
+                modifier = Modifier
+                    .size(imageSize.dp, 400.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .graphicsLayer {
+                        val pageOffset =
+                            ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also {
+                            scaleX = it
+                            scaleY = it
                         }
-                        .clickable(onClick = onClickMovie),
-                    scale = ContentScale.Crop
-                )
-                VerticalSpacer(height = 16.dp)
-                IconMovieTime(textColor = black)
-                VerticalSpacer(height = 8.dp)
-                TextMovieName(state.movies[page].name)
-                VerticalSpacer(height = 16.dp)
-                Row(horizontalArrangement = Arrangement.Center) {
-                    MovieGenre(stringId = R.string.fantasy)
-                    HorizontalSpacer(width = 8.dp)
-                    MovieGenre(stringId = R.string.adventure)
-                }
-                VerticalSpacer(height = 16.dp)
+                    }
+                    .clickable(onClick = onClickMovie),
+                scale = ContentScale.Crop
+            )
+            VerticalSpacer(height = 16.dp)
+            IconMovieTime(textColor = black)
+            VerticalSpacer(height = 8.dp)
+            TextMovieName(state.movies[page].name)
+            VerticalSpacer(height = 16.dp)
+            Row(horizontalArrangement = Arrangement.Center) {
+                MovieGenre(stringId = R.string.fantasy)
+                HorizontalSpacer(width = 8.dp)
+                MovieGenre(stringId = R.string.adventure)
             }
+            VerticalSpacer(height = 16.dp)
         }
-
     }
 
 }
