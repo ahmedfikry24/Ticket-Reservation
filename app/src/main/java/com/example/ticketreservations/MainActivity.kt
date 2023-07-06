bc720ea9ded09ba93c026a0cfb48824624a88553
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-06 20:38:51 +0300

    update MainActivity

--- app/src/main/java/com/example/ticketreservations/MainActivity.kt
+++ app/src/main/java/com/example/ticketreservations/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.ticketreservations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.ticketreservations.screens.movieInfo.MovieInfoScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieInfoScreen()
+           MovieApp()
         }
     }
 }
